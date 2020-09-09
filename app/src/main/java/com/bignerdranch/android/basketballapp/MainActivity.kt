f499dbfecb98099c34fe17a94cdbc2ceff11b15b
mariamedi <19398675+mariamedi@users.noreply.github.com>
2020-09-09 14:54:29 -0400

    added email functionaility

--- app/src/main/java/com/bignerdranch/android/basketballapp/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/basketballapp/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.bignerdranch.android.basketballapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import java.util.*
+
 
 private const val TAG = "MainActivity"
 private const val KEY_A_INDEX = "score_a"
@@ -22,8 +29,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var freeAButton: Button
     private lateinit var freeBButton: Button
     private lateinit var resetButton: Button
+    private lateinit var saveButton: Button
     private lateinit var scoreATextView: TextView
     private lateinit var scoreBTextView: TextView
+    private lateinit var enterTextView: EditText
 
     private val gameViewModel: GameViewModel by lazy {
         ViewModelProviders.of(this).get(GameViewModel::class.java)
@@ -45,8 +54,10 @@ class MainActivity : AppCompatActivity() {
         freeAButton = findViewById(R.id.free_throw_a_button)
         freeBButton = findViewById(R.id.free_throw_b_button)
         resetButton = findViewById(R.id.reset_button)
+        saveButton = findViewById(R.id.save_button)
         scoreATextView = findViewById(R.id.score_a_text)
         scoreBTextView = findViewById(R.id.score_b_text)
+        enterTextView = findViewById(R.id.enter_text)
 
         threeAButton.setOnClickListener { view: View ->
             updateScoreA(3)
@@ -76,6 +87,9 @@ class MainActivity : AppCompatActivity() {
             resetScores()
         }
 
+        saveButton.setOnClickListener { view: View ->
+            sendEmail()
+        }
         val currScoreA = savedInstanceState?.getInt(KEY_A_INDEX, 0) ?: 0
         val currScoreB = savedInstanceState?.getInt(KEY_B_INDEX, 0) ?: 0
 
@@ -141,5 +155,41 @@ class MainActivity : AppCompatActivity() {
         val scoreB = gameViewModel.currentScoreB
         scoreATextView.setText(scoreA.toString())
         scoreBTextView.setText(scoreB.toString())
+    }
+    private fun sendEmail() {
+        val scoreA = gameViewModel.currentScoreA
+        val scoreB = gameViewModel.currentScoreB
+
+        if (!enterTextView.getText().toString().isEmpty()) {
+            val msg = Calendar.getInstance().getTime()
+                .toString() + "\nScore A: " + scoreA.toString() + "\nScore B: " + scoreB.toString()
+            Log.i("Email", "Sending email")
+
+            val TO = arrayOf(enterTextView.getText().toString())
+            val emailIntent = Intent(Intent.ACTION_SEND)
+            emailIntent.data = Uri.parse("mailto:")
+            emailIntent.type = "text/plain"
+
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, TO)
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Game Scores WOO!")
+            emailIntent.putExtra(Intent.EXTRA_TEXT, msg)
+
+            try {
+                startActivity(Intent.createChooser(emailIntent, "Send mail..."))
+                finish()
+                Log.i("Email.", "Sending email")
+            } catch (ex: ActivityNotFoundException) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "There is no email client installed.", Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+        else{
+            Toast.makeText(
+                this@MainActivity,
+                "Enter email.", Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
